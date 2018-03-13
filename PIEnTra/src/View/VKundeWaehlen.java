727769dd5a95cf34e6cd88f14210566da5080c84
joern <joern@X220>
2018-03-13 15:25:51 +0100

    angepasst

--- PIEnTra/src/View/VKundeWaehlen.java
+++ PIEnTra/src/View/VKundeWaehlen.java
@@ -9,14 +9,12 @@ import Controller.CKundeWaehlen;
  * 
  */
 public class VKundeWaehlen extends VKunde {
-
+	private JButton btn_KundeWaehlen;
+    private CKundeWaehlen controller;
+    
     /**
      * Default constructor
      */
     public VKundeWaehlen() {
     }
-
-    private JButton btn_KundeWaehlen;
-    private CKundeWaehlen controller;
-
 }
