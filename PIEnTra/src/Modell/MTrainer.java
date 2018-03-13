39b1d4236c3d5b027d2da3e40466c50dcc3570af
joern <joern@X220>
2018-03-13 16:03:28 +0100

    Getter und Setter gesetzt, Attribute verschoben

--- PIEnTra/src/Modell/MTrainer.java
+++ PIEnTra/src/Modell/MTrainer.java
@@ -12,6 +12,34 @@ public class MTrainer
     /**
      * Default constructor
      */
-    public MTrainer() {
+    public MTrainer()
+    {
+    	
     }
+
+	public int getTrainerID() {
+		return trainerID;
+	}
+
+	public void setTrainerID(int trainerID) {
+		this.trainerID = trainerID;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
 }
