0daec5a9c6202076115abffd2f8abfb32048dfcc
Omega <Teilnehmer@CSC-Akademie_05>
2018-03-15 16:14:38 +0100

    VRESSOURCE Aendern und Waehlen

--- PIEnTra/src/View/VRessourceAendern.java
+++ PIEnTra/src/View/VRessourceAendern.java
@@ -1,6 +1,8 @@
 package View;
 
 
+import javax.swing.JButton;
+
 import java.awt.Container;
 
 import javax.swing.JButton;
@@ -9,25 +11,26 @@ import Controller.CRessourceAendern;
 import Modell.MProdukt;
 
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
 /**
- * 
+ *  Author Leon & Chris
  */
 public class VRessourceAendern extends VRessource 
 {
 	private static final long serialVersionUID = 1L;
-	
-	private JPanel pnl_center;
+
     private CRessourceAendern controller;
-    private JButton btn_Ressource_Aendern;
+    private JButton btn_aendern;
     
+
     public void init()
     {
     	super.init();
     	setVisible(true);
-		//pnl_center.add(btn_Ressource_Aendern = new JButton("Ressourcen Aendern"));
+    	pnl_center.add(btn_aendern = new JButton("Ressource Aendern"));
     }
 	
 	/**
@@ -35,10 +38,13 @@ public class VRessourceAendern extends VRessource
      */
     public VRessourceAendern(CRessourceAendern pController)
     {
+    	
     	controller = pController;
     	init();
+        
     }
-
+    
+    
     public void fillRessource(MProdukt pProdukt) {
      
     }
