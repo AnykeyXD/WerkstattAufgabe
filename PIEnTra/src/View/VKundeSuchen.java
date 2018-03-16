d34fe4b14e666cc73ff8ee358adafd88815fda2f
joern <joern@X220.kln.de.emea.csc.com>
2018-03-16 10:50:06 +0100

    Überflüssige Imports entfernt

--- PIEnTra/src/View/VKundeSuchen.java
+++ PIEnTra/src/View/VKundeSuchen.java
@@ -7,12 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-import View.VProduktDefinieren.btn_produkt_definieren_ActionListener;
-import View.VProduktDefinieren.btn_zurueck_ActionListener;
 import Controller.CKundeSuchen;
 
 /**
