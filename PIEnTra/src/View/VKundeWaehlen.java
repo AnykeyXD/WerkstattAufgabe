000154db5fbe0770ad64ad687bde869eb7af4873
Teilnehmer <Teilnehmer@192.168.5.1>
2018-03-20 14:59:02 +0100

    Kommentare hinzugefügt

--- PIEnTra/src/View/VKundeWaehlen.java
+++ PIEnTra/src/View/VKundeWaehlen.java
@@ -14,8 +14,10 @@ public class VKundeWaehlen extends VKunde
 {
 	private static final long serialVersionUID = 1L;
 	
+	//Buttons deklariert und initialisiert
 	private JButton btn_KundeWaehlen  = new JButton("   Kunde wählen   ");
     private JButton btn_ZurückTraining=new JButton("   Zurück zu Training konfigurieren  ");
+    
 	private CKundeWaehlen controller;
     
     
@@ -31,15 +33,22 @@ public class VKundeWaehlen extends VKunde
     	super.init();
     	super.setTxt_Postion("PlEnTra/KundeWaehlen");
     	
+    	//Button zum panel hinzugefügt
     	pnl_south.add(btn_KundeWaehlen);
     	pnl_south.add(btn_ZurückTraining );
     	
-    	
+    	//Actionlistener hinzugefügt
     	btn_KundeWaehlen.addActionListener(new Btn_kunde_waehlen_ActionListener());
     	btn_ZurückTraining.addActionListener(new Btn_zurück_training_ActionListener());
     	this.setSize(700, 500);    	
     	setVisible(true);
     }
+    /**
+     * ActionListener fuer btn_ressourceWaehlen
+     * 		aktualisiert den Kunden
+     * 
+     * @author Tim Spürkel
+     */
     public class Btn_kunde_waehlen_ActionListener implements ActionListener
     {
     	@Override
@@ -49,6 +58,12 @@ public class VKundeWaehlen extends VKunde
 			
 		}
     }
+    /**
+     * ActionListener fuer btn_ressourceWaehlen
+     * 		aktualisiert den Kunden
+     * 
+     * @author Tim Spürkel
+     */
     public class Btn_zurück_training_ActionListener implements ActionListener
     {
     	@Override
